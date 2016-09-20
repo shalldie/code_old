550ddd91ae8dd1b4405bc1d93ab1765b27471dc5
谢爽 <394229785@qq.com>
2016-09-21 02:26:36 +0800

    add psd file

--- imgtest/js/index.js
+++ imgtest/js/index.js
@@ -1,14 +1,43 @@
+
+// var img = new Image();
+// img.src = 'img/plane.png';
+
+// var canvas = document.getElementById('demo');
+// canvas.width = 500;
+// canvas.height = 300;
+
+// canvas.style.cssText = 'margin:50px auto;display:block;';
+
+// var ctx = canvas.getContext('2d');
+
+// var sWid = 60;
+// var sHei = 180;
+
+// var wid = 170;
+// var hei = 180;
+
+// var x = 0;
+// var y = 730;
+
+// img.onload = function () {
+//     ctx.fillRect(0, 0, wid, hei);
+//     drawImage(ctx, img, x, y, sWid, sHei, 0, 0, wid, hei);
+
+// }
+
+// function drawImage(ctx, image, sX, sY, sWid, sHei, tX, tY, tWid, tHei) {
+//     var offX = (tWid - sWid) / 2;
+
+//     ctx.drawImage(image, sX, sY, sWid, sHei, tX + offX, tY, tWid - offX * 2, tHei);
+// }
+
 var index = 0;
 
-function setIndex(i) {
-    var ele = document.getElementById('demo');
-    ele.style.backgroundPositionX = -i * 180 + 'px';
-    ele.style.backgroundPositionY = 0;
-    i++;
-    if (i >= 11) i = 0;
-    index = i;
-}
+var ele = document.getElementById('demo');
 
-setInterval(function () {
-    setIndex(index);
-}, 100);
+
+// setInterval(function () {
+//     ele.style.backgroundPositionX = - index * 172 + 'px';
+//     index++;
+//     if (index >= 11) index = 0;
+// }, 100);
