e87d98b0c09ef23f8f780bf5ed987b0a328b28a4
谢爽 <394229785@qq.com>
2016-11-21 14:00:37 +0800

    测试 mini-task

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-// require('./footer/app');
+const task = require('mini-task');
+
+let que = task.queue().dequeue();
+
+
+
+for (let i = 0; i < 10; i++) {
+    que.will(n => conosle.log(new Date().getSeconds())).delay(1000);
+}
+
+que.catch(err => console.log(err));
