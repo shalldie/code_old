785bc00edfc29babddaff34eb9eba4dd8091cb47
谢爽 <394229785@qq.com>
2016-11-22 03:14:24 +0800

    测试webpack-dev-server

---
+++ react/src/js/components/demo.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class Demo extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <h2>lalala,终于可以了！的分身乏术反对撒发生的 hahahadfa劳资真是艹了！！！</h2>
+        );
+    }
+}
