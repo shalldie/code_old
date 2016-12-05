3ed08fe4b425f6f36c46966fdf8677b2444bef77
shalldie <394229785@qq.com>
2016-12-05 17:37:27 +0800

    测试fs异步读取

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,13 +1,23 @@
 const fs = require('fs');
 
-let data = '';
+// let str1 = fs.readFileSync('./hello.txt', 'utf-8');
 
-let stream = fs.createReadStream('./hello.txt', 'utf8');
+// let str2 = fs.readFileSync('./jsconfig.json', 'utf-8');
 
-stream.on('data', function (result) {
-    data += result;
-});
 
-stream.on('end', function () {
-    console.log(data);
-});
+
+async function getFile(name) {
+    return new Promise(res => {
+        fs.readFile(name, 'utf-8', (err, result) => {
+            res(result);
+        });
+    });
+}
+
+(async () => {
+    let strPro1 = getFile('./hello.txt');
+
+    let strPro2 = getFile('./jsconfig.json');
+
+    console.log(await strPro1, await strPro2);
+})();
